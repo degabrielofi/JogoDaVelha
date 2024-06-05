83b1279787eba968b76e3673fe3d940365e6bd4f
Gabriel Silva <yzgabrieel02@gmail.com>
2024-06-05 18:37:28 -0300

    add `sweetalert2` in project

--- assets/scripts/interface.js
+++ assets/scripts/interface.js
@@ -1,80 +1,99 @@
 document.addEventListener("DOMContentLoaded", () => {
+  let squares = document.querySelectorAll(".square");
+  let resetRoundBtn = document.querySelector(".resetRound");
+  let resetGameBtn = document.querySelector(".resetGame");
 
-    let squares = document.querySelectorAll(".square");
-    let resetRoundBtn = document.querySelector(".resetRound");
-    let resetGameBtn = document.querySelector(".resetGame");
-   
-    squares.forEach((square) => {
-      
-      square.addEventListener("click", handleClick);
-      
-    });
-  
-    updateScore();
-  
-    resetRoundBtn.addEventListener("click", resetRound);
-    resetGameBtn.addEventListener("click", resetGame);
-  
+  squares.forEach((square) => {
+    square.addEventListener("click", handleClick);
   });
-  
-  function handleClick(event) {
-    
-    let square = event.target;
-    let position = square.id;
-  
-    if(handleMove(position)){
-      setTimeout(() => {
-        if (symbols[playerTime] == "o") {
-          alert("O jogador vencedor foi o: Símbolo!!!");
-          scoreP1++;
-          updateScore();
-          resetRound();
-        }else{
-          alert("O jogador vencedor foi o: Estádio!!!");
-          scoreP2++;
-          updateScore();
-          resetRound();
-        }
-      }, 30); 
-    }
-  
-    if (isDraw()) {
-      setTimeout(() => {
-        alert("O jogo terminou em empatado. Mais sorte da proxima vez!");
+
+  updateScore();
+
+  resetRoundBtn.addEventListener("click", resetRound);
+  resetGameBtn.addEventListener("click", resetGame);
+});
+
+function handleClick(event) {
+  let square = event.target;
+  let position = square.id;
+
+  if (handleMove(position)) {
+    setTimeout(() => {
+      if (symbols[playerTime] == "o") {
+        Swal.fire({
+          title: "Parabéns você venceu!",
+          imageUrl: "./assets/images/SPFC.png",
+          imageWidth: 200,
+          imageHeight: 200,
+          imageAlt: "Custom image",
+          confirmButtonColor: "#8f0100",
+        });
+        scoreP1++;
+        updateScore();
         resetRound();
-      }, 30);
-      
-    }
+      } else {
+        Swal.fire({
+          title: "Parabéns você venceu!",
+          imageUrl: "./assets/images/MorumBIS.png",
+          imageWidth: 200,
+          imageHeight: 200,
+          imageAlt: "Custom image",
+          confirmButtonColor: "#8f0100",
+        });
+        scoreP2++;
+        updateScore();
+        resetRound();
+      }
+    }, 30);
+  }
+
+  if (isDraw()) {
+    setTimeout(() => {
+      Swal.fire({
+        title: "Mais sorte da proxima vez!",
+        text: "O Jogo terminou em empatado.",
+        icon: "question",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Jogar novamente!",
+        cancelButtonText: "Não!",
+      }).then((result) => {
+        if (result.isDismissed) {
+          window.location.href = "https://degabrielofi.vercel.app/";
+        }
+      });
+      resetRound();
+    }, 30);
+  }
+  updateSquare(position);
+}
+
+function updateSquare(position) {
+  let square = document.getElementById(position.toString());
+  let symbol = board[position];
+  square.innerHTML = `<div class="${symbol}"></div>`;
+}
+
+function updateScore() {
+  let player1 = document.querySelector("#p1");
+  let player2 = document.querySelector("#p2");
+  player1.innerText = scoreP1;
+  player2.innerText = scoreP2;
+}
+
+function resetRound() {
+  board.fill("");
+  for (let position = 0; position < board.length; position++) {
     updateSquare(position);
   }
-  
-  function updateSquare(position) {
-    let square = document.getElementById(position.toString());
-    let symbol = board[position];
-    square.innerHTML = `<div class="${symbol}"></div>`;
-  
-  }
-  
-  function updateScore(){
-    let player1 = document.querySelector("#p1");
-    let player2 = document.querySelector("#p2");
-    player1.innerText = scoreP1;
-    player2.innerText = scoreP2;
-  }
-  
-  function resetRound() {
-    board.fill("");
-    for (let position = 0; position < board.length; position++){
-      updateSquare(position);
-    }
-    playerTime = 0;
-    gameOver = false;
-  }
-  
-  function resetGame() {
-    resetRound();
-    scoreP1 = 0;
-    scoreP2 = 0;
-    updateScore();
-    
-  }
+  playerTime = 0;
+  gameOver = false;
+}
+
+function resetGame() {
+  resetRound();
+  scoreP1 = 0;
+  scoreP2 = 0;
+  updateScore();
+}
